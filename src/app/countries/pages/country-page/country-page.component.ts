4e6cbaf8dfa26d6e0934d1599a98cf77494f058a
mdsrad <mdsrad@gmail.com>
2023-05-20 14:23:02 +0200

    primera parte del proyecto de paises, acabado

--- src/app/countries/pages/country-page/country-page.component.ts
+++ src/app/countries/pages/country-page/country-page.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { CountriesService } from '../../services/country.service';
+import { Country } from '../../interfaces/country';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-country-page',
@@ -6,6 +10,27 @@ import { Component } from '@angular/core';
   styles: [
   ]
 })
-export class CountryPageComponent {
+export class CountryPageComponent  implements OnInit{
 
+public country?:Country;
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private countryService: CountriesService,
+    private router: Router,
+    ){}
+
+
+  ngOnInit(): void {
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({id}) => this.countryService.seaarchCountryByAlphaCode( id ))
+    )
+    .subscribe( country => {
+      if( !country ){
+        return this.router.navigateByUrl('')
+      }
+      return this.country = country;
+    });
+  }
 }
